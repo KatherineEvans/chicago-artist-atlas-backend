42debe10938db6abe07056c6ffb26a0104ae4b1f
KatherineEvans <Evans.Katherine.E@gmail.com>
2022-07-01 16:18:29 -0500

    get all theaters

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -1,2 +1,6 @@
 class TheatersController < ApplicationController
+  def index
+    theaters = Theater.all
+    render json: theaters.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
   post "/users" => "users#create"
   get "/users" => "users#index"
+  get "/theaters" => "theaters#index"
 end
