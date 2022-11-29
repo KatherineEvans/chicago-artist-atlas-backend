4f1a2937c25794e461f0fab26ec479a7150ca866
Katherine <Evans.Katherine.E@gmail.com>
2022-11-29 14:38:13 -0600

    updates to seeds file and addition of address/theater database association

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -1,6 +1,6 @@
 class TheatersController < ApplicationController
   def index
     theaters = Theater.all
-    render json: theaters.as_json
+    render json: theaters.as_json(methods: [:address])
   end
 end
--- app/models/theater.rb
+++ app/models/theater.rb
@@ -1,2 +1,3 @@
 class Theater < ApplicationRecord
+  belongs_to :address, optional: true, :foreign_key => "performance_address_id"
 end
