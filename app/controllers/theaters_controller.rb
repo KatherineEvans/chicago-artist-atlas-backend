48bb65035ea66c813ae5b80b33666bfb3541275a
Katherine <Evans.Katherine.E@gmail.com>
2023-02-03 13:44:52 -0600

    add pagination

--- app/controllers/theaters_controller.rb
+++ app/controllers/theaters_controller.rb
@@ -1,6 +1,21 @@
 class TheatersController < ApplicationController
   def index
-    theaters = Theater.all.limit(5)
-    render json: theaters
+    # pagination 
+    search = params[:search]
+    page_number = params[:page_number] || 1
+    page_size = params[:page_size] || 5
+    page_starts_at = ((page_number.to_i - 1) * page_size.to_i)
+
+    if search 
+      theaters = Theater.where("name LIKE ?", "%#{search}%")
+      page_number = 1
+      page_starts_at = ((page_number.to_i - 1) * page_size.to_i)
+    else
+      theaters = Theater.all
+    end
+
+    theaters = theaters.order("name ASC").limit(page_size.to_i).offset(page_starts_at.to_i)
+
+    render json: theaters.as_json
   end
 end
