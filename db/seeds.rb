8c0bc0f6c954c0fab739001481234d94a5746bcf
artroy40 <127887158+artroy40@users.noreply.github.com>
2023-04-12 12:16:51 -0500

    Agencies Finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -427,11 +427,47 @@
 
 # AGENCIES
 
-address = Address.create!(address1: "410 South Michigan Avenue", address2: "Suite 733", city: "Chicago", state: "IL", postal_code: "60605")
+address = Address.create!(address1: "410 South Michigan Ave.", address2: "Suite 733", city: "Chicago", state: "IL", postal_code: "60605")
 wpt = Agency.create!(name: "Actors Talent Group", address_id: address.id, website: "www.actorstalentgroup.com", submission_guidelines: "http://www.actorstalentgroup.com/dark-submission-rules.html", union_status: "SAG/AFTRA")
 
 address = Address.create!(address1: "5100 North Ravenswood", address2: "Suite 102", city: "Chicago", state: "IL", postal_code: "60640")
 wpt = Agency.create!(name: "Big Mouth Talent", address_id: address.id, website: "www.bigmouthtalent.com", submission_guidelines: "https://bigmouthtalent.com/submissions/", union_status: "SAG/AFTRA")
 
-address = Address.create!(address1: "2255 South Michigan Avenue", address2: nil, city: "Chicago", state: "IL", postal_code: "60616")
+address = Address.create!(address1: "2255 South Michigan Ave.", address2: nil, city: "Chicago", state: "IL", postal_code: "60616")
 wpt = Agency.create!(name: "BMG Talent", address_id: address.id, website: "www.bmgtalent.com", submission_guidelines: "https://bmgtalent.com/join", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "3033 N. Clark St.", address2: nil, city: "Chicago", state: "IL", postal_code: "60657")
+wpt = Agency.create!(name: "DDO Talent Agency", address_id: address.id, website: "www.ddoagency.com", submission_guidelines: "http://ddoagency.com/submit/", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "4241 W. 63rd St.", address2: nil, city: "Chicago", state: "IL", postal_code: "60629")
+wpt = Agency.create!(name: "DeSanti Talent Agency", address_id: address.id, website: "www.desantitalents.com", submission_guidelines: "http://www.desantitalents.com/submission.html", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "2558 W. 16th St.", address2: nil, city: "Chicago", state: "IL", postal_code: "60608")
+wpt = Agency.create!(name: "Hayes Talent Agency", address_id: address.id, website: "www.hayestalent.com", submission_guidelines: "https://hayestalent.com/index.php/submission/", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "727 S. Dearborn St.", address2: "Suite 312", city: "Chicago", state: "IL", postal_code: "60605")
+wpt = Agency.create!(name: "Gray Talent Group", address_id: address.id, website: "www.graytalentgroup.com", submission_guidelines: "https://graytalentgroup.com/submission/", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "33 W. Grand Ave.", address2: nil, city: "Chicago", state: "IL", postal_code: "60654")
+wpt = Agency.create!(name: "Grossman & Jack Talent", address_id: address.id, website: "www.grossmanjack.com", submission_guidelines: "https://www.grossmanjack.com/contact-1", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "1017 W. Washington", address2: "Suite 2C", city: "Chicago", state: "IL", postal_code: "60607")
+wpt = Agency.create!(name: "Lily’s Talent Agency, Inc.", address_id: address.id, website: "www.lilystalent.com", submission_guidelines: "http://www.lilystalent.com/apply", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "900 N. Franklin St.", address2: "Suite 709", city: "Chicago", state: "IL", postal_code: "60610")
+wpt = Agency.create!(name: "NV Talent", address_id: address.id, website: "www.nvtalent.com", submission_guidelines: "https://nvtalent.com/submissions/", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "1512 N. Fremont St.", address2: "Suite 105", city: "Chicago", state: "IL", postal_code: "60642")
+wpt = Agency.create!(name: "Paonessa Talent Agency", address_id: address.id, website: "www.paonessatalent.com", submission_guidelines: "https://www.paonessatalent.com/submissions", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "1200 W. 35th St.", address2: "Suite 12110", city: "Chicago", state: "IL", postal_code: "60609")
+wpt = Agency.create!(name: "Promote Talent Agency", address_id: address.id, website: "www.promotetalentagency.com", submission_guidelines: "https://promotetalentagency.com/submissions/", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "159 N. Sangamon St.", address2: "Suite 248", city: "Chicago", state: "IL", postal_code: "60607")
+wpt = Agency.create!(name: "The Rock Agency", address_id: address.id, website: "www.therocktalentagency.com/", submission_guidelines: "https://www.therocktalentagency.com/apply?type=actor", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "333 E. Ontario St.", address2: "Suite 302-B", city: "Chicago", state: "IL", postal_code: "60611")
+wpt = Agency.create!(name: "Shirley Hamilton, Inc.", address_id: address.id, website: "www.shirleyhamiltontalent.com", submission_guidelines: "https://www.shirleyhamiltontalent.com/submissions", union_status: "SAG/AFTRA")
+
+address = Address.create!(address1: "400 N. Michigan", address2: "Suite 700", city: "Chicago", state: "IL", postal_code: "60611")
+wpt = Agency.create!(name: "Stewart Talent", address_id: address.id, website: "www.stewarttalent.com", submission_guidelines: "https://www.stewarttalent.com/site/contact_chicago", union_status: "SAG/AFTRA")
