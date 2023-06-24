988610e61f3bf1edc68bc4b334d628b3ca725fa3
Katherine <Evans.Katherine.E@gmail.com>
2023-06-24 12:33:41 -0500

    adds user_id to awards table because duh

--- db/migrate/20230624160448_create_awards.rb
+++ db/migrate/20230624160448_create_awards.rb
@@ -1,6 +1,7 @@
 class CreateAwards < ActiveRecord::Migration[7.0]
   def change
     create_table :awards do |t|
+      t.integer :user_id
       t.string :name
       t.integer :year
       t.string :url
