985a017139ba90e12b5a0ac6ff7413e2e4c5e607
Katherine <Evans.Katherine.E@gmail.com>
2023-06-24 12:03:31 -0500

    udate resource table

--- db/migrate/20230522170940_create_resources.rb
+++ db/migrate/20230522170940_create_resources.rb
@@ -1,12 +1,14 @@
 class CreateResources < ActiveRecord::Migration[7.0]
   def change
     create_table :resources do |t|
+      t.integer :address_id
       t.string :document_name
       t.string :organization_name
       t.string :pdf_url
       t.string :website_url
-      t.string :type
-      t.string :sub_type
+      t.string :primary_type
+      t.string :secondary_type
+      t.string :tertiary_type
       t.string :description
       t.string :logo
       t.boolean :free_tier
