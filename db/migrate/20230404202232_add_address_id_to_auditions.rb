d92d807dd56c8ed4225c20708d999c12d632efd5
Katherine <Evans.Katherine.E@gmail.com>
2023-08-09 23:22:06 -0500

    adds auditions in seeds file

--- db/migrate/20230404202232_add_address_id_to_auditions.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressIdToAuditions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :auditions, :address_id, :integer
-    remove_column :auditions, :location, :string
-  end
-end
--- db/migrate/20230405001402_add_show_address_id_to_auditions.rb
+++
@@ -1,6 +0,0 @@
-class AddShowAddressIdToAuditions < ActiveRecord::Migration[7.0]
-  def change
-    add_column :auditions, :show_address_id, :integer
-    remove_column :auditions, :show_location
-  end
-end
