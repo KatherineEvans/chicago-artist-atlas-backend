59e8807d6c2ab26d0d38b41857a957db6b59a6f9
Katherine <Evans.Katherine.E@gmail.com>
2023-09-02 19:34:48 -0500

    hopeful fix

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,12 +1,3 @@
 class CharactersController < ApplicationController
 
-  # def create 
-  #   render json: {message: "hey"}
-  # end
-
-  def 
-    user_role = UserRole.find(params[:id])
-    render json: user_role
-  end
-
 end
