51b66c9eafd6e87a9115d9577d1192b42eeaf748
Katherine Evans <kevans@neonone.com>
2024-03-24 16:03:33 -0500

    fix audition address'

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -2,7 +2,7 @@ class Audition < ApplicationRecord
   has_many :characters
   has_many :user_roles, through: :characters
   belongs_to :theater, optional: true
-  belongs_to :audition_address, class_name: "Address", foreign_key: "address_id", optional: true
+  belongs_to :audition_address, class_name: "Address", foreign_key: "audition_address_id", optional: true
   belongs_to :show_address, class_name: "Address", foreign_key: "show_address_id", optional: true 
   belongs_to :callback_address, class_name: "Address", foreign_key: "callback_address_id", optional: true 
 end
