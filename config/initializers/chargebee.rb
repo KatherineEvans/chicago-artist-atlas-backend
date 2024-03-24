e9f257b1d43251899da89b7fdac8366968ba8d0a
Katherine Evans <kevans@neonone.com>
2024-03-24 11:59:08 -0500

    chargebee config

--- config/initializers/chargebee.rb
+++ config/initializers/chargebee.rb
@@ -1,5 +1,5 @@
-site = Rails.env == 'development' ? Rails.application.credentials.local_site : Rails.application.credentials.dev_site
-api_key = Rails.application.credentials.chargebee[:test_full_access]
+site = Rails.env == 'development' ? Rails.application.credentials.local_site : Rails.application.credentials.prod_site
+api_key = 'development' ? Rails.application.credentials.chargebee[:test_full_access] : Rails.application.credentials.chargebee[:live_full_access]
 
 ChargeBee.configure(:site => "#{site}", 
   :api_key => api_key)
